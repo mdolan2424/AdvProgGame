5df9a4bdb94b5694a7c47ba011b18b243ef7f278
Matthew Dolan <mdolan2424@gmail.com>
2015-11-12 19:15:24 -0700

    Item Manager, ItemFactory

---
+++ HunterGame/Game/IDrawable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace HunterGame.Game
+{
+    public interface IDrawable
+    {
+        void Draw(Microsoft.Xna.Framework.Game game, SpriteBatch spriteBatch);
+        void Update(GameTime gametime);
+    }
+}
